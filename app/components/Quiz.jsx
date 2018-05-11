06ee4ddc45bbd1e3df743c21a6b0729c0e4b1b6b
carloscse <carloscsantayana@gmail.com>
2018-05-11 10:32:21 +0200

    Feedback y pasar pregunta

--- app/components/Quiz.jsx
+++ app/components/Quiz.jsx
@@ -29,36 +29,40 @@ export default class Quiz extends React.Component {
   }
 
   onAnswerTrue(){
-    if(this.props.game.index === this.props.quiz.choices.length - 1){
+    if(this.props.game.index === this.props.quiz.choices.length){
       this.props.dispatch(onAnswer(true));
       this.props.dispatch(finish());
     } else {
       this.props.dispatch(onAnswer(true));
     }
+    alert(this.props.quiz.choices[this.props.game.index].feedback);
   }
 
   onAnswerFalse(){
-    if(this.props.game.index === this.props.quiz.choices.length - 1){
+    if(this.props.game.index === this.props.quiz.choices.length){
       this.props.dispatch(onAnswer(false));
       this.props.dispatch(finish());
     } else {
       this.props.dispatch(onAnswer(false));
     }
+    alert(this.props.quiz.choices[this.props.game.index].feedback);
   }
 
   onNextQuestion(){
+    if(this.props.game.nAnswers === this.props.quiz.choices.length){
+      this.props.dispatch(finish());
+    }
     if(this.props.game.index <= this.props.quiz.choices.length - 1){
       this.props.dispatch(next());
-    } else if(this.props.game.nAnswers === this.props.quiz.choices.length){
-      this.props.dispatch(finish());
     }
   }
 
   onPreviousQuestion(){
+    if(this.props.game.nAnswers === this.props.quiz.choices.length){
+      this.props.dispatch(finish());
+    }
     if(this.props.game.index <= this.props.quiz.choices.length - 1){
       this.props.dispatch(previous());
-    } else if(this.props.game.nAnswers === this.props.quiz.choices.length){
-      this.props.dispatch(finish());
     }
   }
 
@@ -74,15 +78,18 @@ export default class Quiz extends React.Component {
     return (
       <div id="container">
         <SCORM dispatch={this.props.dispatch} tracking={this.props.tracking} config={GLOBAL_CONFIG}/>
+        <div className="header">
         <Header user_profile={this.props.user_profile} tracking={this.props.tracking} config={GLOBAL_CONFIG} I18n={I18n} quiz={SAMPLES.quiz_example} game={this.props.game}/>
+        </div>
         <div className="quiz">
+          <h1>{this.props.quiz.choices[this.props.game.index].title}</h1>
           <img className="image" src={this.props.quiz.choices[this.props.game.index].img}/>
           <div className="quizButtonsWrapper">
             <p>
               <button className="next" onClick={this.onNextQuestion.bind(this)}>{I18n.getTrans("i.next")}</button>
               <button className="previous" onClick={this.onPreviousQuestion.bind(this)}>{I18n.getTrans("i.previous")}</button>
-              <button className="answerTrue" onClick={this.onAnswerTrue.bind(this)} disabled={this.state.answered}>{I18n.getTrans("i.true")}</button>
-              <button className="answerFalse" onClick={this.onAnswerFalse.bind(this)} disabled={this.state.answered}>{I18n.getTrans("i.false")}</button>
+              <button className="answerTrue" onClick={this.onAnswerTrue.bind(this)} disabled={this.props.quiz.choices[this.props.game.index].answered === true}>{I18n.getTrans("i.true")}</button>
+              <button className="answerFalse" onClick={this.onAnswerFalse.bind(this)} disabled={this.props.quiz.choices[this.props.game.index].answered === true}>{I18n.getTrans("i.false")}</button>
             </p>
             <p>
               <button className="restart" onClick={this.onRestartQuiz.bind(this)}>{I18n.getTrans("i.restart")}</button>
--- app/reducers/gameReducer.jsx
+++ app/reducers/gameReducer.jsx
@@ -13,18 +13,21 @@ function gameReducer(state = {}, action){
     receivedState = JSON.parse(JSON.stringify(state));
     if(action.response === SAMPLES.quiz_example.choices[receivedState.index].answer){
       receivedState.score += SAMPLES.quiz_example.choices[receivedState.index].score;
-      receivedState.index++;
+      SAMPLES.quiz_example.choices[receivedState.index].answered = true;
       receivedState.nAnswers++;
     } else {
-      receivedState.index++;
+      SAMPLES.quiz_example.choices[receivedState.index].answered = true;
       receivedState.nAnswers++;
     }
     return receivedState;
 
   case 'NEXT_QUESTION':
     receivedState = JSON.parse(JSON.stringify(state));
-    if(receivedState.index <= SAMPLES.quiz_example.choices.length - 1){
-      receivedState.index++;
+    for(let i = 1; i < SAMPLES.quiz_example.choices.length; i++){
+      if((receivedState.index <= SAMPLES.quiz_example.choices.length - 1) && (SAMPLES.quiz_example.choices[receivedState.index + i].answered === false)){
+        receivedState.index += i;
+        break;
+      }
     }
     if(receivedState.index === 4 && receivedState.nAnswers < SAMPLES.quiz_example.choices.length){
       receivedState.index = 0;
@@ -33,8 +36,11 @@ function gameReducer(state = {}, action){
 
   case 'PREVIOUS_QUESTION':
     receivedState = JSON.parse(JSON.stringify(state));
-    if(receivedState.index <= SAMPLES.quiz_example.choices.length - 1 && receivedState.index > 0){
-      receivedState.index--;
+    for(let j = 1; j < SAMPLES.quiz_example.choices.length; j++){
+      if((receivedState.index <= SAMPLES.quiz_example.choices.length - 1) && (receivedState.index > 0) && (SAMPLES.quiz_example.choices[receivedState.index - j].answered === false)){
+        receivedState.index -= j;
+        break;
+      }
     }
     if(receivedState.index === 4 && receivedState.nAnswers < SAMPLES.quiz_example.choices.length){
       receivedState.index = 0;
@@ -50,6 +56,10 @@ function gameReducer(state = {}, action){
     receivedState = JSON.parse(JSON.stringify(state));
     receivedState.index = 0;
     receivedState.score = 0;
+    receivedState.nAnswers = 0;
+    for(let i = 0; i < SAMPLES.quiz_example.choices.length; i++){
+      SAMPLES.quiz_example.choices[i].answered = false;
+    }
     return receivedState;
 
   default:
